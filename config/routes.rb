5ba27800d886202e244af385c711b9fbbbf56561
Salomon Beza <beza.salomon@gmail.com>
2017-02-14 15:55:54 +0200

    Completed Restful actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get "/employees", to: 'employees#index'
+  post"/employees", to: 'employees#create'
+  get "/employees/:id", to:'employees#show'
+  patch "/employees/:id", to:'employees#update'
+  delete "/employees/:id", to:'employees#destroy'
 end
