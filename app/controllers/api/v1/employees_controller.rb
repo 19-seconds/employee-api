ecf15bdfdceaca57488330c759700338a3f5680b
Salomon Beza <beza.salomon@gmail.com>
2017-02-15 10:56:35 +0200

    created v1 view, controller and routes

---
+++ app/controllers/api/v1/employees_controller.rb
@@ -0,0 +1,38 @@
+class Api::V1::EmployeesController < ApplicationController
+  def index
+    @employees = Employee.all
+  end
+
+  def create
+    employee = Employee.create(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      birthdate: params[:birthdate],
+      email: params[:email],
+      ssn: params[:ssn]
+      )
+    render :show
+  end
+
+  def show
+    @employee = Employee.find_by(id:params[:id])
+  end
+
+  def update
+    @employee = Employee.find_by(id:params[:id])
+    @employee.update(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      birthdate: params[:birthdate],
+      email: params[:email],
+      ssn: params[:ssn]
+      )
+    render :show
+  end
+
+  def destroy
+    @employee = Employee.find_by(id:params[:id])
+    @employee.destroy
+    render json: {message: "Employee FIRED!!!!!"}
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,15 @@ Rails.application.routes.draw do
   get "/employees/:id", to:'employees#show'
   patch "/employees/:id", to:'employees#update'
   delete "/employees/:id", to:'employees#destroy'
+
+namespace :api do
+  namespace :v1 do
+    get "/employees", to: 'employees#index'
+    post"/employees", to: 'employees#create'
+    get "/employees/:id", to:'employees#show'
+    patch "/employees/:id", to:'employees#update'
+    delete "/employees/:id", to:'employees#destroy'
+  end
+end
+
 end
